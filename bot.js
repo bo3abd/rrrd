ec5b570a96a8f14d26ff8edcd7edcc65e91723d9
bo3abd <37047946+bo3abd@users.noreply.github.com>
2018-08-07 20:19:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -213,7 +213,7 @@ function play(guild, song) {
 
 
 const adminprefix = ".";
-const devs = ['472052319006228509''472052319006228509'];
+const devs = ['472052319006228509','472052319006228509'];
 client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
     if (!devs.includes(message.author.id)) return;
