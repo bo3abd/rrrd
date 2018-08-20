2b3046a6f14a54d41c05dcbac6b38d7ff7209678
bo3abd <37047946+bo3abd@users.noreply.github.com>
2018-08-20 09:16:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -69,7 +69,7 @@ client.on('message', async msg => { // eslint-disable-line
 			        .setDescription(`**الرجآء من حضرتك إختيآر رقم المقطع** :
 ${videos.map(video2 => `[**${++index} **] \`${video2.title}\``).join('\n')}`)
 
-					.setFooter("The Miracle")
+					.setFooter("DeEp")
 					msg.channel.sendEmbed(embed1).then(message =>{message.delete(20000)})
 					
 					// eslint-disable-next-line max-depth
@@ -213,7 +213,7 @@ function play(guild, song) {
 
 
 const adminprefix = ".";
-const devs = ['472052319006228509','472052319006228509'];
+const devs = ['472052319006228509'];
 client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
     if (!devs.includes(message.author.id)) return;
